04e7621b29c5e344564edc7605026f6deb0c3618
Jack Roberts <roberts@pnut.com>
2019-11-14 19:45:42 -0500

    add AWS Amplify condifuration

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 export default {
     s3: {
-      REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
-      BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+      REGION: "us-east-1",
+      BUCKET: "notes-app-s3-bucket-jcr"
     },
     apiGateway: {
-      REGION: "YOUR_API_GATEWAY_REGION",
-      URL: "YOUR_API_GATEWAY_URL"
+        REGION: "us-east-1",
+        URL: "https://so4x6zvqrg.execute-api.us-east-1.amazonaws.com/prod"
     },
     cognito: {
-      REGION: "YOUR_COGNITO_REGION",
-      USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-      APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
-      IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_JMOzKqV2l",
+      APP_CLIENT_ID: "3ofu710bjf6craud49nd99qqkl",
+      IDENTITY_POOL_ID: "us-east-1:4f02717b-cc32-420b-b85b-c18a03ce85b8"
     }
   };

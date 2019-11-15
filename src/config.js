3545a99011c33142360fff7b5a7d840e1f0b50d6
Jack Roberts <roberts@pnut.com>
2019-11-15 10:03:41 -0500

    Add new note functionality

--- src/config.js
+++ src/config.js
@@ -1,16 +1,17 @@
 export default {
-    s3: {
-      REGION: "us-east-1",
-      BUCKET: "notes-app-s3-bucket-jcr"
-    },
-    apiGateway: {
-        REGION: "us-east-1",
-        URL: "https://so4x6zvqrg.execute-api.us-east-1.amazonaws.com/prod"
-    },
-    cognito: {
-      REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_JMOzKqV2l",
-      APP_CLIENT_ID: "3ofu710bjf6craud49nd99qqkl",
-      IDENTITY_POOL_ID: "us-east-1:4f02717b-cc32-420b-b85b-c18a03ce85b8"
-    }
-  };
+  MAX_ATTACHMENT_SIZE: 5000000,
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-s3-bucket-jcr"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://so4x6zvqrg.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_JMOzKqV2l",
+    APP_CLIENT_ID: "3ofu710bjf6craud49nd99qqkl",
+    IDENTITY_POOL_ID: "us-east-1:4f02717b-cc32-420b-b85b-c18a03ce85b8"
+  }
+};

e9a560a2f64c8d805a99dc883e52cdd81050488d
Jack Roberts <roberts@pnut.com>
2019-11-29 11:35:59 -0500

    create dev and prod environments

--- src/config.js
+++ src/config.js
@@ -1,17 +1,45 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
+//  STRIPE_KEY: "YOUR_STRIPE_DEV_PUBLIC_KEY",
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-s3-bucket-jcr"
+    BUCKET: "jackrobertscom2"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://kxu4fcsi2k.execute-api.us-east-1.amazonaws.com/prod"
+    URL: "https://api.jack-roberts.com/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_JMOzKqV2l",
-    APP_CLIENT_ID: "3ofu710bjf6craud49nd99qqkl",
-    IDENTITY_POOL_ID: "us-east-1:4f02717b-cc32-420b-b85b-c18a03ce85b8"
+    USER_POOL_ID: "us-east-1_3sKOosx98",
+    APP_CLIENT_ID: "64vf8rpikdp51natnuv2fjof31",
+    IDENTITY_POOL_ID: "us-east-1:0372d745-2711-4d02-9a02-a58ae2b7c147"
   }
+};
+
+const prod = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "jackrobertscom2-prod"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://api.jack-roberts.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_8GnBpUCkC",
+    APP_CLIENT_ID: "7thnkijjrs70jra07dgo5t12i8",
+    IDENTITY_POOL_ID: "us-east-1:5f449e36-6274-4e23-b430-271811cb5926"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
